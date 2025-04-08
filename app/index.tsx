50e44b8c5593643ee8fcbb0b52f43cface1e0f7a
peoplexbot <hello@peoplexbot.com>
2025-04-08 14:24:54 -0700

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello WTHISIT 👋</Text>
+    <View style={{ flex: 1, backgroundColor: '#FFDD00', justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 24 }}>🔥 It works! 🔥</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
